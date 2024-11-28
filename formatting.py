a0ebc586cf54a8f36a22aeefa34a29c78b6d9bd5
USBashka <usbashka@gmail.com>
2024-11-28 17:52:36 +0400

    Написан модуль форматирования

---
+++ formatting.py
@@ -0,0 +1,44 @@
+"""Константы и функции, позволяющие форматировать текст в консоли"""
+
+
+
+RESET = "\x1B[0m"  # Сброс форматирования
+
+
+
+class Color:
+    """Цвета"""
+
+    BLACK = "\x1B[30m"
+    RED = "\x1B[31m"
+    GREEN = "\x1B[32m"
+    YELLOW = "\x1B[33m"
+    BLUE = "\x1B[34m"
+    MAGENTA = "\x1B[35m"
+    CYAN = "\x1B[36m"
+    WHITE = "\x1B[37m"
+    RESET = "\x1B[39m"
+
+
+class Style:
+    """Стили текста"""
+
+    B = "\x1B[1m"  # Жирный
+    I = "\x1B[3m"  # Курсив
+    U = "\x1B[4m"  # Подчёркнутый
+    S = "\x1B[9m"  # Зачёркнутый
+
+
+
+def format_titles(titles: str) -> str:
+    """Делает заголовки столбцов таблицы жирными"""
+    
+    return Style.B + titles.replace("|", f"{RESET}|{Style.B}") + RESET
+
+
+
+if __name__ == "__main__":
+    print(f"""Данный модуль позволяет писать в консоли {Style.B}жирным{RESET},
+          {Style.I}курсивом{RESET}, {Style.U}подчёркнутым{RESET} и {Style.S}зачёркнутым{RESET},
+          а также менять {Color.RED}ц{Color.GREEN}в{Color.YELLOW}е{Color.BLUE}т{Color.MAGENTA}а{RESET}!
+          """)
